0e8f3b4c825da2ec4738a1b65970abe45681c097
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-04-22 12:01:55 +0700

    Defind Size by Geometry

--- Program.py
+++ Program.py
@@ -1,3 +1,11 @@
 from tkinter import *
 
 root = Tk()
+root.title("My GUI") # กำหนดชื่อโปรแกรม
+
+# กำหนดขนาดหน้าจอและตำแหน่งหน้าจอ
+root.geometry("600x400+100+200") #กว้าง*ยาว+แกนx+แกน y
+
+
+
+root.mainloop() # ลูปรันวนเรื่อยๆ
